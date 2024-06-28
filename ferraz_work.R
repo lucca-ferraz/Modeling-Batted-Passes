92cacea87623df6f53ce78782af1a0a9116da888
Lucca Ferraz <lbf5@rice.edu>
2024-06-28 15:53:03 -0400

    new plots, updated data to include the Rams

--- ferraz_work.R
+++ ferraz_work.R
@@ -5,8 +5,12 @@ all_plays2023 <- nflreadr::load_pbp()
 library(readxl)
 excel_batted <- read_excel("~/Downloads/CMSACamp/2019-2023 Batted Passes.xlsx")
 excel_batted$is_batted <- 1
+
 excel_simple <- excel_batted |> 
+  mutate(across(c("HOME", "AWAY"), ~ str_replace(
+    string = .x, pattern = "LAR", replacement = "LA"))) |> 
   select(week = WEEK, home_team = HOME, away_team = AWAY, play_id = `PLAY ID`, is_batted)
+
 excel_batted |> 
   filter(YEAR == 2023) |> 
   count()
@@ -97,4 +101,38 @@ pass_plays20192023 |>
        subtitle = "No clear indicator that shorter QBs have more batted balls",
        y = "Batted Pass %", x = "Height")
 
+pass_plays20192023 |> 
+  group_by(passer) |> 
+  summarise(attempts = n(), batted_passes = sum(is_batted), 
+            batted_pass_pct = batted_passes / attempts, 
+            short_pass_pct = sum(pass_length == "short", na.rm = TRUE) / attempts,
+            pass_right_pct = sum(pass_location == "right", na.rm = TRUE) / attempts,
+            pass_left_pct = sum(pass_location == "left", na.rm = TRUE) / attempts,
+            pass_middle_pct = sum(pass_location == "middle", na.rm = TRUE) / attempts,
+            avg_air_yards = sum(air_yards, na.rm = TRUE) / attempts) |> 
+  filter(attempts >= 100) |> 
+  ggplot(aes(short_pass_pct, batted_pass_pct)) +
+  geom_point(alpha = 0.8, color = "dodgerblue4") +
+  ggthemes::theme_clean() +
+  labs(title = "Does Short Passing Correlate With Batted Passes?",
+       subtitle = "Among QBs with at least 100 attempts between 2019 - 2023", 
+       x = "% of Short Passes", y = "% of Batted Balls")
 
+pass_plays20192023 |> 
+  filter(!is.na(pass_location)) |> 
+  group_by(pass_location) |> 
+  summarise(attempts = n(), batted_passes = sum(is_batted), 
+            bat_pct = batted_passes / attempts) |> 
+  ggplot() +
+  geom_col(aes(x = pass_location, y = attempts), fill = "dodgerblue") +
+  geom_col(aes(x = pass_location, y = batted_passes), fill = "red") +
+  geom_label(aes(x = pass_location, y = bat_pct, label = bat_pct))
+
+pass_plays2023 |> 
+  filter(!is.na(pass_location)) |> 
+  group_by(pass_location) |> 
+  summarise(attempts = n(), batted_passes = sum(is_batted), 
+            bat_pct = batted_passes / attempts) |> 
+  ggplot() +
+  geom_col(aes(x = pass_location, y = attempts), fill = "dodgerblue") +
+  geom_col(aes(x = pass_location, y = batted_passes), fill = "red")
